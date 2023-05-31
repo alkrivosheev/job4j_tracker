a21f10b942e612d1ae420230c628a404e134b231
alkrivosheev <alkrivosheev@gmail.com>
2023-05-31 10:01:10 +0300

    CheckstyleBroken

--- src/test/java/ru/job4j/checkstyle/Broken.java
+++ src/test/java/ru/job4j/checkstyle/Broken.java
@@ -2,10 +2,11 @@ package ru.job4j.checkstyle;
 
 public class Broken {
     private int sizeOfEmpty = 10;
-
+    String name;
     public String surname;
-
     public static final String NEWVALUE = "";
+
+    Broken() { }
 
     void echo() { }
 
@@ -16,11 +17,5 @@ public class Broken {
     }
 
     void method(int a, int b, int c, int d, int e, int f, int g) {
-
     }
-
-    String name;
-
-    Broken() { }
-
 }
