2e90c0ba94e4532be141afbc83f0bdf02ca119aa
alkrivosheev <alkrivosheev@gmail.com>
2023-06-01 06:56:32 +0300

    DummyDic

--- src/test/java/ru/job4j/oop/DummyDic.java
+++ src/test/java/ru/job4j/oop/DummyDic.java
@@ -3,8 +3,7 @@ package ru.job4j.oop;
 public class DummyDic {
 
     public String engToRus(String eng) {
-        String res = "Неизвестное слово. " + eng;
-        return res;
+        return "Неизвестное слово. " + eng;
     }
 
     public static void main(String[] args) {
