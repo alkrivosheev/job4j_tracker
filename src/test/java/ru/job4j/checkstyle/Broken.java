c64fa9cb531a5d103b955060117c390a29162eb7
alkrivosheev <alkrivosheev@gmail.com>
2023-06-01 06:38:42 +0300

    Broken

--- src/test/java/ru/job4j/checkstyle/Broken.java
+++ src/test/java/ru/job4j/checkstyle/Broken.java
@@ -2,20 +2,20 @@ package ru.job4j.checkstyle;
 
 public class Broken {
     public static final String NEWVALUE = "";
-    String name;
-    public String surname;
+    private String name;
+    private String surname;
     private int sizeOfEmpty = 10;
 
-    Broken() { }
+    public Broken() { }
 
-    void echo() { }
+    public void echo() { }
 
-    void media(Object obj) {
+   public void media(Object obj) {
         if (obj != null) {
             System.out.println(obj);
         }
     }
 
-    void method(int a, int b, int c, int d, int e, int f, int g) {
+   public void method(int a, int b, int c, int d, int e, int f, int g) {
     }
 }
