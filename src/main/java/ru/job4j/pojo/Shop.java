de1008b2720bfb3c231d8a30273011e0b5761266
alkrivosheev <68105349+alkrivosheev@users.noreply.github.com>
2023-06-12 14:02:26 +0300

    ShopDrop&Test

--- src/main/java/ru/job4j/pojo/Shop.java
+++ src/main/java/ru/job4j/pojo/Shop.java
@@ -16,7 +16,7 @@ public class Shop {
     public static void main(String[] args) {
         Product[] products = new Product[5];
         products[0] = new Product("Milk", 10);
-        products[1] = new Product("Bread", 4);
+        products[1] = new Product("Milk", 10);
         products[2] = new Product("Egg", 19);
         for (int i = 0; i < products.length; i++) {
             Product product = products[i];
@@ -24,5 +24,7 @@ public class Shop {
                 System.out.println(product.getName());
             }
         }
+        boolean eq = products[0].equals(products[1]);
+        System.out.println(eq);
     }
 }
