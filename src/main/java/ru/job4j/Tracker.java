34d1baee7868cf1a8afeb68fde4a9cc5f6cdabc6
alkrivosheev <alkrivosheev@gmail.com>
2023-06-13 14:16:39 +0300

    Tracker&Test

--- src/main/java/ru/job4j/Tracker.java
+++ src/main/java/ru/job4j/Tracker.java
@@ -4,8 +4,19 @@ import java.util.Arrays;
 
 public class Tracker {
     private final Item[] items = new Item[100];
-    private int ids = 1;
+    private int ids = 0;
     private int size = 0;
+
+    private int indexOf(int id) {
+        int rsl = -1;
+        for (int index = 0; index < size; index++) {
+            if (items[index].getId() == id) {
+                rsl = index;
+                break;
+            }
+        }
+        return rsl;
+    }
 
     public Item add(Item item) {
         item.setId(ids++);
@@ -14,15 +25,8 @@ public class Tracker {
     }
 
     public Item findById(int id) {
-        Item rsl = null;
-        for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
-                break;
-            }
-        }
-        return rsl;
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
     }
 
     public Item[] findAll() {
@@ -40,5 +44,15 @@ public class Tracker {
             }
         }
         return Arrays.copyOf(rsl, tmpSize);
+    }
+
+    public boolean replace(int id, Item item) {
+        int index = indexOf(id);
+        if (index != -1) {
+            items[index] = item;
+            return true;
+        }
+        return false;
+
     }
 }
