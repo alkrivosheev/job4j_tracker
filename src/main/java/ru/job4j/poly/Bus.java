8056ada11c56877dad2ecc975e14d5d7042e646a
alkrivosheev <alkrivosheev@gmail.com>
2023-06-20 07:03:00 +0300

    Bus

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -3,16 +3,17 @@ package ru.job4j.poly;
 public class Bus implements Transport {
     @Override
     public void drive() {
-
+        System.out.println("Поездка.");
     }
 
     @Override
     public void passengers(int count) {
-
+        System.out.println("Посадка пассажиров.");
     }
 
     @Override
     public double refuel(double count) {
+        System.out.println("Заправка.");
         return 0;
     }
 }
