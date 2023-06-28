c2a80c2528fd14069f7a351567c914ff73c139ad
alkrivosheev <alkrivosheev@gmail.com>
2023-06-28 06:54:47 +0300

    StartUITest

--- src/test/java/ru/job4j/StartUITest.java
+++ src/test/java/ru/job4j/StartUITest.java
@@ -66,8 +66,9 @@ public class StartUITest {
         };
         new StartUI(out).init(in, tracker, actions);
         assertThat(out.toString()).isEqualTo(
-                "Menu." + System.lineSeparator()
-                        + "0. Exit" + System.lineSeparator()
+                "Menu:" + System.lineSeparator()
+                        + "0. Exit Program" + System.lineSeparator()
+                + "=== Exit Program ===" + System.lineSeparator()
         );
     }
 
