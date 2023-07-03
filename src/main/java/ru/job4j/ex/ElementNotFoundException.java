82f8868469473a0c18cd771fa71fbc7ccb68828d
alkrivosheev <alkrivosheev@gmail.com>
2023-07-03 13:33:57 +0300

    FindEl

--- src/main/java/ru/job4j/ex/ElementNotFoundException.java
+++ src/main/java/ru/job4j/ex/ElementNotFoundException.java
@@ -1,7 +1,8 @@
 package ru.job4j.ex;
 
 public class ElementNotFoundException extends Exception {
-    public ElementNotFoundException() {
-        System.out.println("Исключение: Строка не найдена.");
+
+    public ElementNotFoundException(String message) {
+        super(message);
     }
 }
--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -10,7 +10,7 @@ public class FindEl {
             }
         }
         if (rsl == -1) {
-            throw new ElementNotFoundException();
+            throw new ElementNotFoundException("Исключение: Строка не найдена.");
         }
         return rsl;
     }
