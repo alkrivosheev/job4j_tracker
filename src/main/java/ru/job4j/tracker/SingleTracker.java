65fe334c131defac8363fdbf436631299b0a9ed0
alex <alkrivosheev@gmail.com>
2023-07-10 06:54:46 -0400

    SingleTracker

---
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -0,0 +1,42 @@
+package ru.job4j.tracker;
+
+import ru.job4j.Item;
+import ru.job4j.Tracker;
+
+public class SingleTracker {
+    private static Tracker tracker = null;
+
+    private SingleTracker() {
+    }
+
+    public static Tracker getInstance() {
+        if (tracker == null) {
+            tracker = new Tracker();
+        }
+        return tracker;
+    }
+
+    public Item add(Item item) {
+        return tracker.add(item);
+    }
+
+    public Item findById(int id) {
+        return tracker.findById(id);
+    }
+
+    public Item[] findAll() {
+        return tracker.findAll();
+    }
+
+    public Item[] findByName(String key) {
+        return tracker.findByName(key);
+    }
+
+    public boolean replace(int id, Item item) {
+        return tracker.replace(id, item);
+    }
+
+    public boolean delete(int id) {
+        return tracker.delete(id);
+    }
+}
