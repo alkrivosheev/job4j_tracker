acc2d9bf38f7f03f8d0e6706244c1c83131fdb0c
alkrivosheev <alkrivosheev@gmail.com>
2023-08-28 06:57:50 +0300

    UserCompareTo

--- src/main/java/ru/job4j/Item.java
+++ src/main/java/ru/job4j/Item.java
@@ -49,8 +49,8 @@ public class Item {
     public String toString() {
         return "Item{"
                 + "id=" + id
-                + ", name='" + name + '\''
-                + ", created=" + created.format(FORMATTER)
+//                + ", name='" + name + '\''
+//                + ", created=" + created.format(FORMATTER)
                 + '}';
     }
 
