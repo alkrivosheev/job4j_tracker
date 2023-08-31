2755b71469665c97b638f7ff32e5e5653cbe65b8
alkrivosheev <alkrivosheev@gmail.com>
2023-08-31 11:49:51 +0300

    StrintCompare&Test

--- src/main/java/ru/job4j/collection/StringCompare.java
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -5,19 +5,11 @@ import java.util.Comparator;
 public class StringCompare implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
-        int leftLn = left.length();
-        int rightLn = right.length();
-        int minLn = Math.min(leftLn, rightLn);
-        for (int i = 0; i < minLn; i++) {
-            int asciiOfs1 = (int) left.charAt(i);
-            int asciiOfs2 = (int) right.charAt(i);
-            if (asciiOfs1 != asciiOfs2) {
-                return asciiOfs1 - asciiOfs2;
+        for (int i = 0; i < Math.min(left.length(), right.length()); i++) {
+            if (left.charAt(i) != right.charAt(i)) {
+                return Character.compare(left.charAt(i), right.charAt(i));
             }
         }
-        if (leftLn != rightLn) {
-            return leftLn - rightLn;
-        }
-        return 0;
+        return Integer.compare(left.length(), right.length());
     }
 }
