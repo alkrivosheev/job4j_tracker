e38718b4424c4dafc4af74857b1357894d14b19c
alkrivosheev <68105349+alkrivosheev@users.noreply.github.com>
2023-09-11 18:37:00 +0300

    CollectionTask

--- src/main/java/ru/job4j/collection/CollectionTask.java
+++ src/main/java/ru/job4j/collection/CollectionTask.java
@@ -5,8 +5,8 @@ import java.util.TreeSet;
 
 public class CollectionTask {
     public static void main(String[] args) {
-        int[] mass1 = {4,3,2,1};
-        int[] mass2 = {4,5};
+        int[] mass1 = {4, 3, 2, 1};
+        int[] mass2 = {4, 5};
         Set<Integer> resMass = new TreeSet<>();
         for (int num : mass1) {
             resMass.add(num);
