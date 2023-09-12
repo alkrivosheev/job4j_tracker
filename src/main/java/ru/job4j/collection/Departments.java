cc4466e6299d0c5d128d2d3edad95dad798d0f29
alkrivosheev <alkrivosheev@gmail.com>
2023-09-12 15:34:19 +0300

    Departments

---
+++ src/main/java/ru/job4j/collection/Departments.java
@@ -0,0 +1,25 @@
+package ru.job4j.collection;
+
+import java.util.*;
+
+public class Departments {
+    public static List<String> fillGaps(List<String> deps) {
+        Set<String> tmp = new LinkedHashSet<>();
+        for (String value : deps) {
+            String start = value.split("/", 2)[0];
+            for (String el : value.split("/")) {
+                tmp.add(start + "/" + el);
+            }
+        }
+        return new ArrayList<>(tmp);
+    }
+
+    public static void sortAsc(List<String> orgs) {
+        Collections.sort(orgs);
+    }
+
+    public static void sortDesc(List<String> orgs) {
+        Comparator<String> cmp = new DepDescComp();
+        Collections.sort(orgs, cmp);
+    }
+}
