8a0d90163edf22db90771bf420fbc3ae1a73829c
alkrivosheev <alkrivosheev@gmail.com>
2023-09-14 06:40:54 +0300

    Departments

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -5,10 +5,7 @@ import java.util.Comparator;
 public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
-        if (o1.split("/", 2)[0].compareTo(o2.split("/", 2)[0]) == 0) {
-            return o1.compareTo(o2);
-        } else {
-            return o2.compareTo(o1);
-        }
+        int result = o2.split("/", 2)[0].compareTo(o1.split("/", 2)[0]);
+        return result != 0 ? result : o1.compareTo(o2);
     }
 }
