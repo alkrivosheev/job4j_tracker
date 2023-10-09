dfce1137686c9b64b433d02667c902f1da425adb
alkrivosheev <alkrivosheev@gmail.com>
2023-10-09 12:31:42 +0300

    FunctionComposition

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -1,6 +1,7 @@
 package ru.job4j.search;
 
 import java.util.ArrayList;
+import java.util.function.Predicate;
 
 public class PhoneDictionary {
     private ArrayList<Person> persons = new ArrayList<Person>();
@@ -11,9 +12,14 @@ public class PhoneDictionary {
 
     public ArrayList<Person> find(String key) {
         ArrayList<Person> result = new ArrayList<>();
+        Predicate<Person> compName = n -> n.getName().equals(key);
+        Predicate<Person> compSurName = sn -> sn.getSurname().equals(key);
+        Predicate<Person> compPhone = cp -> cp.getPhone().contains(key);
+        Predicate<Person> compAddress = ca -> ca.getAddress().contains(key);
+        Predicate<Person> combine = compName.or(compSurName).or(compPhone).or(compAddress);
+
         for (Person person : persons) {
-            if (person.getName().contains(key) || person.getPhone().contains(key)
-                    || person.getSurname().contains(key) || person.getAddress().contains(key)) {
+            if (combine.test(person)) {
                 result.add(person);
             }
         }
