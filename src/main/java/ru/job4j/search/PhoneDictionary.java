5af546e9d747683f0d14de5f5e4a62963e861115
alkrivosheev <68105349+alkrivosheev@users.noreply.github.com>
2023-10-09 19:48:58 +0300

    Lambda

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -17,7 +17,6 @@ public class PhoneDictionary {
         Predicate<Person> compPhone = cp -> cp.getPhone().contains(key);
         Predicate<Person> compAddress = ca -> ca.getAddress().contains(key);
         Predicate<Person> combine = compName.or(compSurName).or(compPhone).or(compAddress);
-
         for (Person person : persons) {
             if (combine.test(person)) {
                 result.add(person);
