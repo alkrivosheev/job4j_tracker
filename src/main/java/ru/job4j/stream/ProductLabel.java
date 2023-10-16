ae48d230eb284216a18dc3350fe468ff30733222
alkrivosheev <alkrivosheev@gmail.com>
2023-10-16 12:28:05 +0300

    StreamUsage

---
+++ src/main/java/ru/job4j/stream/ProductLabel.java
@@ -0,0 +1,16 @@
+package ru.job4j.stream;
+
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class ProductLabel {
+    public List<String> generateLabels(List<Product> products) {
+        return products.stream()
+                .filter(product -> product.getStandard() - product.getActual() <= 3)
+                .filter(product -> product.getStandard() - product.getActual() >= 0)
+                .map(product -> new Label(product.getName(), product.getPrice() / 2))
+                .map(i -> i.toString()).collect(Collectors.toList());
+    }
+
+}
