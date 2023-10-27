2799d7ad9683378dc8278d45227d5ac9a52738f5
alkrivosheev <alkrivosheev@gmail.com>
2023-10-27 06:42:14 +0300

    DepartmentsTest List.of

--- src/test/java/ru/job4j/collection/DepartmentsTest.java
+++ src/test/java/ru/job4j/collection/DepartmentsTest.java
@@ -10,7 +10,7 @@ public class DepartmentsTest {
     @Test
     public void whenMissed() {
         List<String> input = Arrays.asList("k1/sk1", "k2/sk1");
-        List<String> expect = Arrays.asList("k1", "k1/sk1", "k2", "k2/sk1");
+        List<String> expect = List.of("k1", "k1/sk1", "k2", "k2/sk1");
         List<String> result = Departments.fillGaps(input);
         assertThat(result).containsSequence(expect);
     }
@@ -18,7 +18,7 @@ public class DepartmentsTest {
     @Test
     public void whenNonChange() {
         List<String> input = Arrays.asList("k1", "k1/sk1");
-        List<String> expect = Arrays.asList("k1", "k1/sk1");
+        List<String> expect = List.of("k1", "k1/sk1");
         List<String> result = Departments.fillGaps(input);
         assertThat(result).containsSequence(expect);
     }
@@ -30,7 +30,7 @@ public class DepartmentsTest {
                 "K1/SK1/SSK2",
                 "K2/SK1/SSK1"
         );
-        List<String> expect = Arrays.asList(
+        List<String> expect = List.of(
                 "K1",
                 "K1/SK1",
                 "K1/SK1/SSK1",
@@ -56,7 +56,7 @@ public class DepartmentsTest {
                 "K2/SK1/SSK2",
                 "K2/SK1/SSK1"
         );
-        List<String> expect = Arrays.asList(
+        List<String> expect = List.of(
                 "K1",
                 "K1/SK1",
                 "K1/SK1/SSK1",
@@ -81,7 +81,7 @@ public class DepartmentsTest {
                 "K2/SK1/SSK2",
                 "K2/SK1/SSK1"
         );
-        List<String> expect = Arrays.asList(
+        List<String> expect = List.of(
                 "K1/SK1/SSK1",
                 "K1/SK1/SSK2",
                 "K1/SK2",
@@ -106,7 +106,7 @@ public class DepartmentsTest {
                 "K2/SK1/SSK2",
                 "K2/SK1/SSK1"
         );
-        List<String> expect = Arrays.asList(
+        List<String> expect = List.of(
                 "K2",
                 "K2/SK1",
                 "K2/SK1/SSK1",
@@ -132,7 +132,7 @@ public class DepartmentsTest {
                 "K2/SK1/SSK2",
                 "K2/SK1/SSK1"
         );
-        List<String> expect = Arrays.asList(
+        List<String> expect = List.of(
                 "K2/SK1",
                 "K2/SK1/SSK1",
                 "K2/SK1/SSK2",
