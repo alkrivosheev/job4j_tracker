db39f757434601ddd27841d266f337b916abac29
alkrivosheev <68105349+alkrivosheev@users.noreply.github.com>
2024-10-24 17:01:48 +0300

    sort

--- src/test/java/ru/job4j/algo/sort/MergeTest.java
+++ src/test/java/ru/job4j/algo/sort/MergeTest.java
@@ -7,7 +7,7 @@ import static org.assertj.core.api.Assertions.*;
 public class MergeTest {
 
     @Test
-    void whenSortedThenOk(){
+    void whenSortedThenOk() {
         int[] array = {10, 4, 6, 4, 8, -13, 2, 3};
         assertThat(Merge.mergesort(array)).containsExactly(-13, 2, 3, 4, 4, 6, 8, 10);
     }
