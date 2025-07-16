b5c3313067dacd6684b439b135ea615f7afd33c0
alkrivosheev <alkrivosheev@gmail.com>
2025-07-16 07:30:32 +0300

    add lombok

--- src/main/java/ru/job4j/lombok/Category.java
+++ src/main/java/ru/job4j/lombok/Category.java
@@ -2,15 +2,18 @@ package ru.job4j.lombok;
 
 import lombok.*;
 
-@RequiredArgsConstructor
+
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Category {
     @Getter
     @EqualsAndHashCode.Include
-    @NonNull
     private int id;
 
     @Getter
     @Setter
     private String name;
+
+    public Category(int id) {
+        this.id = id;
+    }
 }
